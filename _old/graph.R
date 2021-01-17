92b94d4f92314b1b29a873077a0772c4b7ae0ff8
NanoScholar <sachini.wijesena1@gmail.com>
2021-01-17 17:40:22 +1100

    test

--- _old/graph.R
+++ _old/graph.R
@@ -49,3 +49,59 @@ df = px.data.tips()
 fig = px.histogram(df, x="total_bill", nbins=20)
 fig.show()
 
+
+
+
+
+#start again
+
+
+data("property_prices")
+#data <- property_prices
+#data
+#m<-ls(data)
+#m
+
+y<-data[,"sale_price"]
+x<-data[,"sale_qtr"]
+
+fig <- plot_ly(data, y=~sale_price, x=~sale_qtr, type = 'scatter', mode = 'markers')
+
+fig <- plot_ly(data, y=~sale_price, x=~sale_qtr, type = 'scatter', mode = 'markers')
+
+
+fig <- plot_ly(data, y=~sale_price/sale_qtr, x=~sale_qtr, type = 'histogram')
+fig
+
+summary(x)
+
+p1 <- plot_ly(data, y = ~sale_price, type = "histogram")
+p1 
+
+p2 <- plot_ly(y = ~sale_qtr, type = "histogram")
+p2 
+
+p3 <- plot_ly(y = ~income, type = "histogram")
+p3 
+
+p4 <- plot_ly(y = ~lot_size, type = "histogram")
+p4 
+
+
+
+# Simple Bar Plot
+counts <- table(data$sale_qtr)
+barplot(counts, main="sale_qtr",
+        xlab="sale_qtr")
+
+
+vars <- names(data)
+plots <- list()
+for(var in vars) {
+  counts <- table(data[[var]]) #data[, p, with = FALSE]
+  plots[[var]] <- function() {barplot(counts, main = var, xlab = var)}
+}
+plots$income()
+
+a<- plot(1:2, 1:2)
+a 
